b23f7a2b4f2cc228338e62a549115246c7c380e7
AKissMail <andreas@kissmehl>
2023-02-24 18:19:13 +0100

    Setup of the footer and header. Also, a dummy component for the content.

--- aar4923_var23_04/src/App.js
+++ aar4923_var23_04/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './style/app.css';
+import Header from "./component/header";
+import Footer from "./component/footer";
+import Content from "./component/content";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+        <Content />
+      <Footer />
     </div>
   );
 }
---
+++ aar4923_var23_04/src/component/content.js
@@ -0,0 +1,9 @@
+import "./../style/content.css";
+function Content() {
+    return(
+        <div className='content'>
+            Conceptual todo
+        </div>
+    )
+}
+export default Content;
